b43d3ceae5ccb4d4472105bfb96c68c5b348fc74
VaskinaYana <vaskinayana866@gmail.com>
2026-02-27 07:42:35 +0500

    lab1

---
+++ Laba 1/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+let check number = 
+    [
+        for i in 1.. number do
+            printf "введите число: "
+            let number = int(Console.ReadLine())
+            if number = 0
+            then yield 0
+            else yield number / 2
+    ]
+
+[<EntryPoint>]
+let main args = 
+    printfn "введите кол-во чисел для списка"
+    let number = int(Console.ReadLine())
+    printfn "список:  %A" (check number)
+    0
---
+++ Задание 2/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let rec check number =
+    if number < 0 then
+        check (abs number)  
+    elif number < 10 then  
+        number % 2 = 0      
+    else check (number / 10)  
+
+[<EntryPoint>]
+let main args = 
+    printfn "Введите число:"
+    let number = int(Console.ReadLine())
+    printfn "Результат: %A" (check number)
+    0
